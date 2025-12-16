0869645237b32e1496ada236c6340951d9536621
CRBroughton <crbroughton@posteo.uk>
2025-12-16 20:06:49 +0000

    create clojure flake

---
+++ clojure/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        packages = [
+          pkgs.clojure # The Clojure CLI tools (`clj`)
+          pkgs.jdk25_headless # Java Development Kit 25 (headless)
+          pkgs.git # Always useful for development
+        ];
+
+        envPackage = pkgs.buildEnv {
+          name = "clojure-dev";
+          paths = packages;
+        };
+
+      in
+      {
+        packages.default = envPackage;
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+        };
+      }
+    );
+}
