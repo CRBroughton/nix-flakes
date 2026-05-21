00648d64d6d3499b64679de1b88457fb5bfc308f
CRBroughton <crbroughton@posteo.uk>
2026-05-21 20:05:26 +0100

    add actual hello world to clojure bb

--- clojure-bb/src/core.clj
+++ clojure-bb/src/core.clj
@@ -1,4 +1,17 @@
 (ns core)
 
-(defn greet [name]
+(defn greet
+  "Returns a greeting string for the given name."
+  [name]
   (str "Hello, " name "!"))
+
+(defn -main
+  "Entry point. Greets the first CLI arg, or 'World' if none provided."
+  [& args]
+  (-> args
+      first
+      (or "World")
+      greet
+      println))
+
+(-main)
